2c17ff28bdab85ad8539abe9b2a3aa7ea1cfa929
Danny Wahl <danny@iywahl.com>
2025-01-28 15:21:57 -0700

    1.0.1

--- Formula/ezgif.rb
+++ Formula/ezgif.rb
@@ -1,8 +1,8 @@
 class Ezgif < Formula
   desc "Optimized FFMPEG wrapper for creating gifs."
   homepage "https://github.com/thedannywahl/ezgif"
-  url "https://github.com/thedannywahl/ezgif/archive/refs/tags/1.0.0.zip"
-  sha256 "a3bffd87b652a09c1cc4f2010c25dcd4c00a31b8b27b9b2a4615b716db027cd4"
+  url "https://github.com/thedannywahl/ezgif/archive/refs/tags/1.0.1.zip"
+  sha256 "1fa7e61dccd2e518d5f717271d6826b44d2edae5d65eeb6f64e5d3d9bd8d2b05"
   license "Apache-2.0"
 
   depends_on "ffmpeg"
